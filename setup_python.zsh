3e0b3c31cccd3a43a8dfdc5f24b39c70cefb0d5f
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2021-08-04 12:56:26 -0300

    Add setup_node.zsh and matching Dotbot command
    Install Python 3.9.5 and conda 4.10.3 with miniforge3-4.10.1-5 via pyenv.

---
+++ setup_python.zsh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Python Setup >>>\n"
+
+# Python versions are managed with `pyenv`, which is in the Brewfile.
+# See zshrc for PYENV_ROOT and PYENV_SHELL variables, 
+#   addition to $path array and occult pyenv() function.
+
+if [ $(pyenv versions --bare) ]; then
+  echo "Python $(pyenv versions --bare) already installed"
+else
+  echo "Installing Python & conda with pyenv..."
+  pyenv install miniforge3-4.10.1-5
+  pyenv global miniforge3-4.10.1-5
+fi
+
+echo "Global Python Versions Installed:"
+pyenv versions
