0ed78a6c1789ce5ebf040e6aa7a436a144a4d11d
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2021-08-23 14:00:20 -0300

    Add docker-machine with xhyve configuration

--- setup_virtualization.zsh
+++ setup_virtualization.zsh
@@ -11,3 +11,12 @@ else
   # https://forums.docker.com/t/docker-command-not-found-after-installing-docker-desktop-on-mac/93837/2
   open /Applications/Docker.app
 fi
+
+if [ "root" != $(stat -f '%Su' /usr/local/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve) ] || \
+   [ "wheel" != $(stat -f '%Sg' /usr/local/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve) ] || \
+   [ "-r-sr-xr-x" != $(stat -f '%Sp' /usr/local/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve) ]
+then
+  echo "Enter superuser (sudo) password to xhyve driver to access the hypervisor."
+  sudo chown root:wheel /usr/local/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+  sudo chmod u+s /usr/local/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+fi
