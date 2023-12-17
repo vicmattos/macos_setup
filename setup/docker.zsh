0a5fd7a22260aba656bbf78b0ae83f376ec4be6c
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2023-12-17 16:13:21 -0300

    Configure `docker-compose` as docker command

--- setup/docker.zsh
+++ setup/docker.zsh
@@ -1,2 +1,6 @@
 
-mkdir -p /Applications/Docker.app/Contents/Resources/cli-plugins
+echo "\n<<< Starting Docker Setup >>>\n"
+
+# Compose is now a Docker plugin. For Docker to find this plugin, symlink it:
+mkdir -p ~/.docker/cli-plugins
+ln -sfn /usr/local/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose
